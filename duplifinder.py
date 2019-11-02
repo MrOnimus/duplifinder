4e37540ed9a6d20aad76fbdb10fc7769a4bfde91
Gleb <gleb.gugl@gmail.com>
2019-11-03 02:35:42 +0300

    Now the output is nice, and also there is a function to get modification datetime.

--- duplifinder.py
+++ duplifinder.py
@@ -2,9 +2,10 @@ import hashlib
 import os
 import sys
 import traceback
+import platform
 
 
-def addDictElem(arr, dictionary):
+def add_elem_to_dict(arr, dictionary):
     key = arr[0]
     val = arr[1]
 
@@ -14,11 +15,12 @@ def addDictElem(arr, dictionary):
     return dictionary
 
 
-def getHashofFilesInDir(argv, verbose=0):
+def get_hash_of_files_in_dir(argv, verbose=0):
     directory = str(argv[0])
-    hashTable = {}
+    hash_table = {}
 
     if not os.path.exists(directory):
+        print('This is not a valid directory.')
         return -1
 
     try:
@@ -33,15 +35,15 @@ def getHashofFilesInDir(argv, verbose=0):
                     # You can't open the file for some reason
                     f1.close()
                     continue
-                fileHash = hashlib.sha256()
+                file_hash = hashlib.sha256()
                 while 1:
                     # Read file in as little chunks
                     buf = file.read(4096)
                     if not buf:
                         break
-                    fileHash.update(buf)
-                    hashTable = addDictElem(
-                        [fileHash.hexdigest(), filepath], hashTable)
+                    file_hash.update(buf)
+                    hash_table = add_elem_to_dict(
+                        [file_hash.hexdigest(), filepath], hash_table)
                 file.close()
 
     except:
@@ -49,24 +51,35 @@ def getHashofFilesInDir(argv, verbose=0):
         traceback.print_exc()
         return -2
 
-    return hashTable
+    return hash_table
 
 
-def displayUsage():
+def creation_date(path_to_file):
+    if platform.system() == 'Windows':
+        return os.path.getmtime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        return stat.st_mtime
+
+
+def display_usage():
     print('usage: duplifinder <path to directory>')
 
 
-def displayResult(dictionary):
-    print('You have duplicates here:')
+def display_result(dictionary):
+    #print('You have ' + str(len(dictionary)) + ' here:')
     for key in dictionary:
         if len(dictionary[key]) > 1:
-            print(dictionary[key])
+            print('This files are duplicates:')
+            for i, dup_elem in enumerate(dictionary[key]):
+                print('  ' + str(i + 1) + '. ' + dup_elem)
+            print('')
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) > 0:
-        result = getHashofFilesInDir(args, 0)
-        displayResult(result)
+        result = get_hash_of_files_in_dir(args, 0)
+        display_result(result)
     else:
-        displayUsage()
+        display_usage()
