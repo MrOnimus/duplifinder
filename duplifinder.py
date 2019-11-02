98f879a6e564574369dcfb1686112cdc4c4a2996
Gleb <gleb.gugl@gmail.com>
2019-11-02 07:23:08 +0300

    Added usage and corrected display of results.

--- duplifinder.py
+++ duplifinder.py
@@ -1,56 +1,67 @@
-import hashlib, os, sys
+import hashlib
+import os
+import sys
+import traceback
+
 
 def addDictElem(arr, dictionary):
-	if arr[0] in dictionary:
-		dictionary[arr[0]].append(arr[1])
-	else:
-		dictionary[arr[0]] = []
-		dictionary[arr[0]].append(arr[1])
-	return dictionary
+    key = arr[0]
+    val = arr[1]
+
+    if key not in dictionary:
+        dictionary[key] = []
+    dictionary[key].append(val)
+    return dictionary
+
 
 def getHashofFilesInDir(directory, verbose=0):
-	hashTable = {}
-	if not os.path.exists (directory):
-		return -1
+    hashTable = {}
 
-	try:
-		for root, dirs, files in os.walk(directory):
-			for names in files:
-				if verbose == 1:
-					print('Hashing', names)
-				filepath = os.path.join(root,names)
-				try:
-					file = open(filepath, 'rb')
-				except:
-					# You can't open the file for some reason
-					f1.close()
-					continue
-				fileHash = hashlib.sha256()
-				while 1:
-					# Read file in as little chunks
-					buf = file.read(4096)
-					if not buf:
-						break
-					fileHash.update(buf)
-					hashTable = addDictElem([fileHash.hexdigest(), filepath], hashTable)
-				file.close()
+    if not os.path.exists (directory):
+        return -1
 
-	except:
-		import traceback
-		# Print the stack traceback
-		traceback.print_exc()
-		return -2
+    try:
+        for root, dirs, files in os.walk(directory):
+            for names in files:
+                if verbose == 1:
+                    print('Hashing', names)
+                filepath = os.path.join(root,names)
+                try:
+                    file = open(filepath, 'rb')
+                except:
+                    # You can't open the file for some reason
+                    f1.close()
+                    continue
+                fileHash = hashlib.sha256()
+                while 1:
+                    # Read file in as little chunks
+                    buf = file.read(4096)
+                    if not buf:
+                        break
+                    fileHash.update(buf)
+                    hashTable = addDictElem([fileHash.hexdigest(), filepath], hashTable)
+                file.close()
 
-	return hashTable
+    except:
+        # Print the stack traceback
+        traceback.print_exc()
+        return -2
+
+    return hashTable
 
 def displayUsage():
-	print('usage: duplifinder <path to directory>')
+    print('usage: duplifinder <path to directory>')
+
+def displayResult(dictionary):
+    print('You have duplicates here:')
+    for key in dictionary:
+        if len(dictionary[key]) > 1:
+            print(dictionary[key])
 
 if __name__ == "__main__":
-	if len(sys.argv[1:]) == 1:
-		getHashofFilesInDir('.', 1)
-	else:
-		displayUsage
+    if len(sys.argv[1:]) > 0:
+        result = getHashofFilesInDir('.', 0)
+        displayResult(result)
+    else:
+        displayUsage()
 
-result = getHashofFilesInDir('.', 1)
-print(result)
