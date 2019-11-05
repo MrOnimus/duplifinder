2c0a4d8c61e5e4ebe83e704fb1dc6af4483c4171
Gleb <gleb.gugl@gmail.com>
2019-11-05 14:38:44 +0300

    Cleaned up the code from comments and meaningless stuff

--- duplifinder.py
+++ duplifinder.py
@@ -39,20 +39,16 @@ def get_hash_of_files_in_dir(argv, verbose=0):
                     file.close()
                     continue
                 file_hash = hashlib.sha256()
-                while 1:
-                    # Read file in as little chunks
+                while True:
                     buf = file.read(4096)
-                    if not buf:
-                        break
+                    if not buf: break
                     file_hash.update(buf)
                     hash_table = add_elem_to_dict(
                         [file_hash.hexdigest(), filepath], hash_table)
                 file.close()
 
     except:
-        # Print the stack traceback
-        traceback.print_exc()
-        return -2
+        return -1
 
     return hash_table
 
@@ -73,9 +69,10 @@ def display_usage():
     print('usage: duplifinder <path to directory>')
 
 
-#def is_somewhat_unixtime(dictionary, elem, callback):
-#    for key in dictionary:
-#        if len(dictionary[key]):
+def is_this_unixtime_min(list_of_duplicates, elem):
+    if min(list_of_duplicates, key = lambda elem : elem['unixtime']):
+        return True
+    return False
 
 
 def filter_the_dictionary(dictionary):
@@ -103,29 +100,22 @@ def form_query_for_format(list_of_duplicates):
 
 
 def print_small_table(list_of_duplicates):
-    #max_path_len = get_max_path_len(list_of_duplicates)
     query = form_query_for_format(list_of_duplicates)
     for elem in list_of_duplicates:
         path = elem['path']
         datetime = get_readable_datetime(elem['unixtime'])
         print(query.format(path, datetime))
-    #    if max_path_len < 35:
-    #        print("{:35s} {:10s}".format(path, datetime))
-    #    elif max_path_len < 80:
-    #        print("{:80s} {:10s}".format(path, datetime))
-    #    else:
-    #        print("{:120s} {:10s}".format(path, datetime))
 
 
 def display_result(dictionary):
-    #print('You have ' + str(len(dictionary)) + ' here:')
     f_dictionary = filter_the_dictionary(dictionary)
+    print('You have ' + str(len(f_dictionary)) + ' set of duplicates here:')
     if len(f_dictionary) > 0:
         for key in f_dictionary:
             if len(f_dictionary[key]) > 1:
                 print('This files are duplicates:')
-                for dup_elem in f_dictionary[key]:
-                    dup_elem['unixtime'] = modification_date(dup_elem['path'])
+                for elem in f_dictionary[key]:
+                    elem['unixtime'] = modification_date(elem['path'])
                 print_small_table(f_dictionary[key])
                 print('')
     else:
@@ -133,17 +123,15 @@ def display_result(dictionary):
 
 
 def display_error(result):
-    if result == -1:
-        print("Can't read the file.")
-    elif result == -2:
-        print("Something went wrong.")
+    print("Something went wrong.")
+    traceback.print_exc()
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) > 0:
         result = get_hash_of_files_in_dir(args, 0)
-        if result != -1 and result != -2:
+        if result != -1:
             display_result(result)
         else:
             display_error(result)
